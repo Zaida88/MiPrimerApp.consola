f454292a11e7a15f8dab68c012956179ca2757ed
Zaida88 <zsm.mejia@yavirac.edu.ec>
2021-06-30 14:00:49 -0500

    PROYECTO INTEGRADOR

---
+++ PROYECTO INTEGRADOR/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoIntegrador
+{
+    public class Usuario
+    {
+        public int Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public int Edad { get; set; }
+    }
+
+}
